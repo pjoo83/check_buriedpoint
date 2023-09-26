ed3b1f55f99ff68f2c3d0586768167450b77fa7f
changhao <hao.chang@ushow.media>
2023-09-26 17:24:27 +0800

    提交启动APP方法

--- testcase/uiautomaitor.py
+++ testcase/uiautomaitor.py
@@ -7,10 +7,13 @@ class test_IOS:
     def __init__(self):
         self.driver = GetDriver_u2().create_driver(init_driver_android_ui2())
 
-    def star_app(self):
+    def test01_star_app(self):
         self.driver.open_app('com.starmakerinteractive.starmaker')
 
+    def test_02_test_live(self):
+        pass
 
-test_IOS().star_app()
+
+
 
 
