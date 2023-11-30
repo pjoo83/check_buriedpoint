52ad4ac10da5e7a16429c0965a115f466e057d66
pjoo83 <pjoo83@163.com>
2023-11-30 14:32:45 +0800

    注释appium部分

--- Base/get_driver.py
+++ Base/get_driver.py
@@ -1,17 +1,17 @@
-from appium import webdriver
-
+# from appium import webdriver
+#
 from Base.base_uiautomator import Element
 
 
-class GetDriver:
-    def __init__(self):
-        self.driver = None
-
-    def create_driver(self, desired_caps):
-        if not self.driver:
-            self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-
-            return self.driver
+# class GetDriver:
+#     def __init__(self):
+#         self.driver = None
+#
+#     def create_driver(self, desired_caps):
+#         if not self.driver:
+#             self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+#
+#             return self.driver
 
 
 class GetDriver_u2:
